84e692a1b026e1f2ed5c5b4c1b73542cf2c772e4
Chandra Sukmagalih Arifin <chandrachansa@gmail.com>
2024-01-07 01:25:48 +0700

    ..

--- src/app/component/receipt/receipt.component.ts
+++ src/app/component/receipt/receipt.component.ts
@@ -10,17 +10,33 @@ import jsPDF from 'jspdf';
 export class ReceiptComponent implements OnInit {
     currentDate: Date = new Date();
     orders: any[] = [];
+    taxesRatio: number = 0.1; //10%
+    taxes: number = 0;
+    subTotal: number = 0;
+    total: number = 0;
+    receivedAmount: number = 200000;
+    changes: number = 0;
 
     constructor() {}
 
     ngOnInit(): void {
         for (let i = 0; i < 10; i++) {
             this.orders.push({
-              name: `Products ${i + 1}`,
-              qty: i+1,
+                name: `Products ${i + 1}`,
+                qty: i + 1,
                 price: 2890 * (i + 1) + i
             });
         }
+        this.countTotal();
+    }
+
+    countTotal() {
+        for (const order of this.orders) {
+            this.subTotal += order.price;
+        }
+        this.taxes = this.subTotal * this.taxesRatio;
+        this.total = this.subTotal + this.taxes;
+        this.changes = this.receivedAmount - this.total;
     }
 
     convertToPdf() {
