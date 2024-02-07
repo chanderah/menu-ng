d76a1badb1061f3c5e3eddb78f054b07667a9ae7
Chandra Sukmagalih Arifin <chandrachansa@gmail.com>
2024-02-07 21:35:18 +0700

    ..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     appUrl: 'https://menu-ng.vercel.app',
     // appUrl: 'https://chandrasa.fun',
-    apiUrl: 'https://go.chandrasa.fun/api',
+    apiUrl: 'https://api.chandrasa.fun/api',
     publicPath: 'https://chandrasa.fun/public',
     imagePath: 'assets/images',
     firebaseConfig: {
