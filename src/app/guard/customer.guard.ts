ae59b43a055337fe5a3a7d22b67b59398d7cef68
Chandra Sukmagalih Arifin <chandrachansa@gmail.com>
2025-04-07 01:00:58 +0700

    update

--- src/app/guard/customer.guard.ts
+++ src/app/guard/customer.guard.ts
@@ -33,7 +33,7 @@ export class CustomerGuard implements CanActivate {
         // invalid table param
       }
 
-      this.router.navigateByUrl('/');
+      this.router.navigateByUrl('/', { replaceUrl: true });
       return true;
     } else if (!this.sharedService.isLoggedIn && !this.customerService.isCustomer) {
       this.router.navigateByUrl('/customer', { skipLocationChange: true });
