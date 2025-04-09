d1a64101fc1c40425fa1bf001cd111e412bd0e16
Chandra Sukmagalih Arifin <chandra.sukmagalih@bankmandiri.co.id>
2025-04-09 10:54:04 +0700

    update

---
+++ src/app/storage.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Aes256Service } from './service/aes256.service';
+import { jsonParse, jsonStringify } from './lib/utils';
+import { Nullable } from './interface/common';
+
+interface DataWithExpiry<T = any> {
+  data: T;
+  expiry: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  constructor(private aes256Service: Aes256Service) {}
+
+  get<T = any>(key: string): Nullable<T> {
+    const localData = localStorage.getItem(key);
+    if (!localData) return null;
+
+    const value = this.aes256Service.decrypt(localData);
+    return jsonParse(value) as T;
+  }
+
+  set(key: string, data: any) {
+    const value = this.aes256Service.encrypt(jsonStringify(data));
+    localStorage.setItem(key, value);
+  }
+
+  getWithExpiry<T = any>(key: string): Nullable<T> {
+    const localData = localStorage.getItem(key);
+    if (!localData) return null;
+
+    const { data, expiry } = jsonParse<DataWithExpiry<T>>(localData);
+    if (Date.now() > expiry) {
+      localStorage.removeItem(key);
+      return null;
+    }
+    return data;
+  }
+
+  setWithExpiry(key: string, data: any, expiryInMinutes: number = 720) {
+    this.set(key, {
+      data,
+      expiry: Date.now() + expiryInMinutes * 60 * 1000,
+    });
+  }
+}
